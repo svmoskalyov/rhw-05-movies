e47dfe531d6b18a421d4aeeaad7ea83cfeacbf32
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-03-07 11:27:34 +0200

    Create Box.js

---
+++ src/components/Box.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import {
+  space,
+  layout,
+  flexbox,
+  grid,
+  position,
+  typography,
+  color,
+  border,
+  shadow,
+} from 'styled-system';
+
+export const Box = styled('div')(
+  space,
+  layout,
+  flexbox,
+  grid,
+  position,
+  typography,
+  color,
+  border,
+  shadow
+);
