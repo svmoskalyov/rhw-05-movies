edf6d9baf5b92b34eb62add67290ab900aeef166
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-03-07 11:26:45 +0200

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,44 @@
+export const theme = {
+  colors: {
+    black: '#000',
+    white: '#fff',
+    text: '#2a2a2a',
+    background: '#fff',
+    primary: '#3f51b5',
+    secondary: '#303f9f',
+    accent: '#609',
+    muted: '#a4a4a4',
+    online: '#2ac22f',
+    offline: '#ee301f',
+  },
+  space: [0, 2, 4, 8, 16, 24, 32, 64, 128, 256],
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'system-ui, sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '18px',
+    l: '32px',
+    xl: '64px',
+  },
+  fontWeights: {
+    normal: 500,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  radii: {
+    none: '0',
+    normal: '4px',
+    round: '50%',
+  },
+};
