0b3409f6307de59bce57635243b59f986448f710
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-03-11 17:58:39 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,51 +1,73 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+const API_KEY = '634387309f6bc4d8e17fedbf5dc6be14';
 
 export const getTrending = async () => {
-  const response = await axios.get('trending/movie/day?', {
-    params: {
-      api_key: '634387309f6bc4d8e17fedbf5dc6be14',
-    },
-  });
-  return response.data.results;
+  try {
+    const response = await axios.get('trending/movie/day?', {
+      params: {
+        api_key: API_KEY,
+      },
+    });
+    return response.data.results;
+  } catch (error) {
+    toast.error(error.message);
+  }
 };
 
 export const getSearch = async query => {
-  const response = await axios.get('search/movie?', {
-    params: {
-      api_key: '634387309f6bc4d8e17fedbf5dc6be14',
-      query,
-    },
-  });
-  return response.data.results;
+  try {
+    const response = await axios.get('search/movie?', {
+      params: {
+        api_key: API_KEY,
+        query,
+      },
+    });
+    return response.data.results;
+  } catch (error) {
+    toast.error(error.message);
+  }
 };
 
 export const getMovieId = async id => {
-  const response = await axios.get(`movie/${id}?`, {
-    params: {
-      api_key: '634387309f6bc4d8e17fedbf5dc6be14',
-    },
-  });
-  return response.data;
+  try {
+    const response = await axios.get(`movie/${id}?`, {
+      params: {
+        api_key: API_KEY,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    toast.error(error.message);
+  }
 };
 
 export const getCasts = async id => {
-  const response = await axios.get(`movie/${id}/credits?`, {
-    params: {
-      api_key: '634387309f6bc4d8e17fedbf5dc6be14',
-    },
-  });
-  return response.data.cast;
+  try {
+    const response = await axios.get(`movie/${id}/credits?`, {
+      params: {
+        api_key: API_KEY,
+      },
+    });
+    return response.data.cast;
+  } catch (error) {
+    toast.error(error.message);
+  }
 };
 
 export const getReviws = async id => {
-  const response = await axios.get(`movie/${id}/reviews?`, {
-    params: {
-      api_key: '634387309f6bc4d8e17fedbf5dc6be14',
-    },
-  });
-  return response.data.results;
+  try {
+    const response = await axios.get(`movie/${id}/reviews?`, {
+      params: {
+        api_key: API_KEY,
+      },
+    });
+    return response.data.results;
+  } catch (error) {
+    toast.error(error.message);
+  }
 };
 
 /*
