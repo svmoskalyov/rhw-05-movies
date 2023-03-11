c7581eeca8f4069cdb3622d606e12fab65ef13fc
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-03-11 17:59:55 +0200

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,7 +1,10 @@
-import { Box } from 'components/Box';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { Box } from 'components/Box';
 import { getCasts } from 'services/api';
+import { Loader } from 'components/Loader/Loader';
+import { List, Item, Name, Character } from './Cast.styled';
+import NoImage from 'images/no-image-available.png';
 
 export const Cast = () => {
   const { movieId } = useParams();
@@ -13,14 +16,26 @@ export const Cast = () => {
 
   return (
     <Box p={3}>
-      {casts.length === 0 && <div>loading...</div>}
-      {casts.map(({ id, name, character, profile_path }) => (
-        <li key={id}>
-          <img src={profile_path} alt={name} width="240" />
-          {name}
-          <p>Character: {character}</p>
-        </li>
-      ))}
+      {casts.length === 0 && <Loader />}
+      <List>
+        {casts.map(({ id, name, character, profile_path }) => (
+          <Item key={id}>
+            <img
+              loading="lazy"
+              src={
+                !profile_path
+                  ? `${NoImage}`
+                  : `https://image.tmdb.org/t/p/w200${profile_path}`
+              }
+              alt={name}
+              width="200"
+              height="300"
+            />
+            <Name>{name}</Name>
+            <Character>Character: {character}</Character>
+          </Item>
+        ))}
+      </List>
     </Box>
   );
 };
