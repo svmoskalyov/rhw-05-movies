ab430955f5a16493da4b111231328d9f32715e92
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-03-11 17:59:39 +0200

    Create Movies.styled.js

---
+++ src/pages/Movies/Movies.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const List = styled.ul``;
+
+export const Item = styled.li`
+  padding: ${p => p.theme.space[1]}px 0;
+  font-size: ${p => p.theme.fontSizes.m};
+`;
