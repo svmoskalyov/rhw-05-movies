f50a108e0b8b12794b64d6af512a9d1394b06289
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-03-12 13:03:03 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -69,23 +69,3 @@ export const getReviws = async id => {
     toast.error(error.message);
   }
 };
-
-/*
-const THE_KEY = '634387309f6bc4d8e17fedbf5dc6be14';
-
-
-https://api.themoviedb.org/3/trending/movie/day?api_key=THE_KEY
-
-https://api.themoviedb.org/3/search/movie?api_key=THE_KEY&query=comedy
-https://api.themoviedb.org/3/search/movie?api_key=THE_KEY&query={query}
-
-https://api.themoviedb.org/3/movie/505642?api_key=THE_KEY
-https://api.themoviedb.org/3/movie/{movie_id}?api_key=THE_KEY
-
-https://api.themoviedb.org/3/movie/505642/credits?api_key=THE_KEY
-https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=THE_KEY
-
-https://api.themoviedb.org/3/movie/505642/reviews?api_key=THE_KEY
-https://api.themoviedb.org/3/movie/{movie_id}/reviews?api_key=THE_KEY
-
-*/
